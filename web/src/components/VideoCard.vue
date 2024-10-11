99e5333a491330dd5bfdebcd6e9f476f89eb39e0
Jake Stanley <prudentia713@gmail.com>
2024-10-11 02:21:36 +0100

    switch front end to vue3

---
+++ web/src/components/VideoCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="video-card">
+    <video class="video-element" controls :src="videoSrc"></video>
+    <div class="video-text">
+      <h3>{{ title }}</h3>
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideoCard',
+  props: {
+    videoSrc: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      default: 'Untitled Video'
+    },
+    description: {
+      type: String,
+      default: 'No description available.'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.video-card {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  background-color: #fff;
+}
+
+.video-element {
+  width: 100%;
+  height: auto;
+  border-radius: 4px;
+}
+
+.video-text {
+  margin-top: 10px;
+}
+
+.video-text h3 {
+  margin: 0;
+  font-size: 1.2em;
+}
+
+.video-text p {
+  margin: 5px 0 0;
+  color: #555;
+}
+</style>
---
+++ web/src/views/VideosView.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="videos">
+    <h1>This the videos page</h1>
+    <div class="video-grid">
+      <VideoCard
+        v-for="(video, index) in videos"
+        :key="index"
+        :video-src="video.src"
+        :title="video.title"
+        :description="video.description"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import VideoCard from '../components/VideoCard.vue'
+
+export default {
+  name: 'Videos',
+  components: {
+    VideoCard
+  },
+  data() {
+    // return {
+    //   videos: [
+    //     {
+    //       src: 'videos/video1.mp4',
+    //       title: 'Introduction to Vue.js',
+    //       description: 'Learn the basics of Vue.js in this introductory video.'
+    //     },
+    //     {
+    //       src: 'videos/video2.mp4',
+    //       title: 'Advanced Vue Techniques',
+    //       description: 'Delve deeper into Vue.js with advanced concepts.'
+    //     }
+    //     // Add more video objects as needed
+    //   ]
+    // }
+    return {
+      videos: [],
+    }
+  },
+  created() {
+    this.fetchVideos();
+  },
+  methods: {
+    async fetchVideos() {
+      
+    }
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+
+.video-grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+}
+</style>
