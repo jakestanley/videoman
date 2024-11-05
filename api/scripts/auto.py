60d001a7ebf2b95f1ebaeb584103ff12c7b5dd01
Jacob Stanley <>
2024-11-05 12:42:56 +0000

    actually use assume_tags in auto script

--- api/scripts/auto.py
+++ api/scripts/auto.py
@@ -3,7 +3,7 @@ from api.db import start_redis_server, stop_redis_server
 from api.args import get_args
 from api.tags import get_resources_without_tags
 from api.files import get_videos_by_ids
-from api.cache import get_cache_dir
+from api.auto import assume_tags
 
 def start():
     print("Running auto")
@@ -13,7 +13,8 @@ def start():
     untagged = get_resources_without_tags()
     videos = get_videos_by_ids(untagged[0:10])
     for video in videos:
-        print(video)
+        tags = assume_tags(video)
+        print(f"Assumed tags for video %s: %s" % (video["relative_path"], tags))
     stop_redis_server()
 
 if __name__ == "__main__":
